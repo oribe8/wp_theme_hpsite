6dc05ce45e8a2cad518db32a9814cca66ec7915b
oribe8 <grasslight193@gmail.com>
2024-08-21 09:45:30 +0900

    oldフォルダ削除

--- _old/js/script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// -----------ハンバーガーメニュー-----------
-document.getElementById('hamburgericon').onclick = function() {
-    // ハンバーガーメニューの展開
-    document.querySelector('#spnav ul').classList.toggle("is-active");
-    // アイコン変形
-    document.querySelector('#hamburgericon span').classList.toggle("is-active");
-}
